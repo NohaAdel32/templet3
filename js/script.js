47ea47674aef7df4c3cfa07c7083a2d4b93693cc
NohaAdel32 <love.queen1921989@gmail.com>
2024-11-12 23:17:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text ");
+    var attrValue = element.getAttribute("data-bs-title");
+    element.setAttribute("data-bs-title", "Copied");
+  }
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+   
